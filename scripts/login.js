8ef513b994504a078becff47f453f49521cdf1d1
Alec Mirambeau <alecmirambeau@gmail.com>
2025-09-09 12:33:08 -0400

    forgot to commit js code

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,8 @@
 
 function login() {
     const loginForm = document.getElementsByClassName("login");
+    // we could use the class to get the elements. First lets console.log(loginForm.elements) to see what it gives us. Maybe we can use the DOM tree
+    // probs wouldn't be used IRL because anytime we change the DOM we would need to update this JS code.
     
+
 }
